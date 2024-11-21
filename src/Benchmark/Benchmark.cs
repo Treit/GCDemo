2c588c428658b862ad6d3b5d8934d284944dcd56
Mike Treit (from Dev Box) <mtreit@ntdev.microsoft.com>
2024-11-21 15:24:24 -0800

    Add a benchmark.

---
+++ src/Benchmark/Benchmark.cs
@@ -0,0 +1,50 @@
+﻿namespace Test
+{
+    using BenchmarkDotNet.Attributes;
+    using System;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+
+    [MemoryDiagnoser]
+    public class Benchmark
+    {
+        HttpClient _client;
+
+        [Params(10, 100, 1000)]
+        public int Count { get; set; }
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            _client = new HttpClient();
+        }
+
+        [Benchmark(Baseline = true)]
+        public async Task<int> FromPrefix()
+        {
+            var totalProcessed = 0;
+
+            for (int i = 0; i < Count; i++)
+            {
+                var result = await _client.GetAsync("http://localhost:5257/fromPrefix?prefix=a&count=1000");
+                totalProcessed++;
+            }
+
+            return totalProcessed;
+        }
+
+        [Benchmark]
+        public async Task<int> FromPrefixToList()
+        {
+            var totalProcessed = 0;
+
+            for (int i = 0; i < Count; i++)
+            {
+                var result = await _client.GetAsync("http://localhost:5257/fromPrefixToList?prefix=a&count=1000");
+                totalProcessed++;
+            }
+
+            return totalProcessed;
+        }        
+    }
+}
---
+++ src/Benchmark/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Test
+{
+    using BenchmarkDotNet.Running;
+    using System;
+    using System.Threading.Tasks;
+
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+#if RELEASE
+            BenchmarkRunner.Run<Benchmark>();
+#else
+            Benchmark b = new Benchmark();
+            b.Count = 100;
+            b.GlobalSetup();
+            var first = await b.FromPrefix();
+            var second = await b.FromPrefixToList();
+            Console.WriteLine(first);
+            Console.WriteLine(second);
+#endif
+
+        }
+    }
+}

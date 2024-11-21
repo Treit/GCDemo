79a4dafe01073b1309a987b5f7e8a1634ce5ea57
Mike Treit <mtreit@ntdev.microsoft.com>
2024-11-20 16:13:03 -0800

    Clean up the code.

--- src/DemoService/Program.cs
+++ src/DemoService/Program.cs
@@ -1,8 +1,4 @@
 using KTrie;
-using Microsoft.AspNetCore.Http.HttpResults;
-using System.IO.MemoryMappedFiles;
-
-var callCount = 0;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,28 +20,8 @@ foreach (var word in words)
     trie.Add(word);
 }
 
-app.MapGet("/ping", (HttpContext context) =>
-{
-    Interlocked.Increment(ref callCount);
-    byte[] bytes = [];
-
-    if (context.Request.Query.ContainsKey("size"))
-    {
-        bytes = new byte[int.Parse(context.Request.Query["size"]!)];
-    }
-
-    if (callCount % 10 == 0 && context.Request.Query.ContainsKey("gc"))
-    {
-        GC.Collect();
-    }
-
-    return "pong - " + bytes.Length;
-});
-
 app.MapGet("/fromPrefix", (HttpContext context) =>
 {
-    Interlocked.Increment(ref callCount);
-
     var prefix = context.Request.Query["prefix"].FirstOrDefault();
     var countStr = context.Request.Query["count"].FirstOrDefault();
 
@@ -66,8 +42,6 @@ app.MapGet("/fromPrefix", (HttpContext context) =>
 
 app.MapGet("/fromPrefixToList", (HttpContext context) =>
 {
-    Interlocked.Increment(ref callCount);
-
     var prefix = context.Request.Query["prefix"].FirstOrDefault();
     var countStr = context.Request.Query["count"].FirstOrDefault();
 
@@ -86,44 +60,4 @@ app.MapGet("/fromPrefixToList", (HttpContext context) =>
     return Results.Ok(matches);
 });
 
-DoMonitorResources("MonitorResources", CancellationToken.None);
-
 app.Run();
-
-static void DoMonitorResources(string operation, CancellationToken cancelToken)
-{
-    GC.RegisterForFullGCNotification(10, 10);
-    Console.Title = operation;
-
-    _ = Task.Factory.StartNew(
-        () =>
-        MonitorResources(cancelToken),
-        cancelToken,
-        TaskCreationOptions.LongRunning,
-        TaskScheduler.Default);
-}
-
-static void MonitorResources(CancellationToken ct)
-{
-    var sleepTime = 500;
-    var commfile = "demo_comm";
-    using var mmf = MemoryMappedFile.CreateOrOpen(commfile, 8);
-    using var view = mmf.CreateViewAccessor(0, 8);
-
-    view.Write(4, true);
-
-    while (!ct.IsCancellationRequested)
-    {
-        try
-        {
-            Thread.Sleep(sleepTime);
-            GC.Collect();
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine($"OH NO {e}");
-        }
-    }
-
-    view.Write(0, -1);
-}
